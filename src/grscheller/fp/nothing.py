c39871c0ff2939e1c87184b27d2a9e612dcd407a
grscheller <geoffrey@scheller.com>
2024-08-08 09:26:20 -0600

    Cannot use for more strongly typed code
    * Undermines more strongly typed code with its use of `Any`
    * When I tried to tighten up the typing my[py] internally broke!
      * the __getattr__ method is the problem
    * Maybe move this implementation to new PyPI project called untyped
    * Have some ideas of using an XOR implementation
      * put them in the class Nothing docstring

--- src/grscheller/fp/nothing.py
+++ src/grscheller/fp/nothing.py
@@ -34,7 +34,6 @@ __license__ = "Apache License 2.0"
 from typing import Any, Callable, Iterator, Optional, TypeVar
 
 T = TypeVar('T')
-S = TypeVar('S')
 
 class Nothing():
     """Singleton semantically represents a missing value.
@@ -44,6 +43,26 @@ class Nothing():
     * returns itself if called as a Callable with arbitrary arguments
     * interpreted as an empty container by standard Python functions
     * makes for a better "bottom type" than either None or ()
+
+    ## TODO: Fundamental redesign is needed!!!
+
+    * Undermines more strongly typed code which uses this Class
+    * When I tried to tighten up the typing my[py] internally broke!
+    * Maybe I will move this implementation to new PyPI project called untyped
+      * ideas going forward:
+        * inherit from or contain an XOR
+          * catch exceptions and return a right containing the exception
+          * give it a dict so users can add relevant methods???
+        * let it be an XOR (or subtype of XOR)
+          * Possibly have XOR(left: Never, right: ~R, timeout = ???)
+            * Python does have a TimeoutError
+          * XOR(left: ~L, right: ~E|~MB) produces a "left" value
+          * XOR(left: Never, right: ~E) produces a "right" containing an exception
+          * XOR(left: Never, right: MB(timeout)) produces a "right" containing an exception
+          * for when left is in infinite loop?
+          * catch all exceptions from left and bundle them in a right
+      * potentialNothing(left) = XOR(left, MB()) 
+      * nothing = potentialNothing = XOR(MB(), ~E|Literal[MB()])
 
     """
     __slots__ = ()
@@ -83,16 +102,16 @@ class Nothing():
     def __setitem__(self, index: int|slice, item: Any) -> None:
         return
 
-    def __getattr__(self, name: str) -> Any:
-        def method(*args: Any, **kwargs: Any) -> Callable[[Any], Any]:
-            return Nothing()
-        return method
+#   def __getattr__(self, name: str) -> Any:
+#       def method(*args: Any, **kwargs: Any) -> Callable[[Any], Any]:
+#           return Nothing()
+#       return method
 
     def __call__(*args: Any, **kwargs: Any) -> Any:
         return Nothing()
 
-    def get(self, alt: Optional[T]=None) -> T|Nothing:
-        """Return an alternate value of type ~T or a Nothing."""
+    def get(self, alt: Optional[Any]=None) -> Any:
+        """Return an alternate value if a Nothing."""
         if alt is None:
             return Nothing()
         else:
