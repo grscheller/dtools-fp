65b66e179372374bbc91df2d62e4f5cbc7a9466e
grscheller <geoffrey@scheller.com>
2024-12-10 14:52:02 -0700

    Finished tests for fp.function

--- tests/grscheller/fp/function/test_function.py
+++ tests/grscheller/fp/function/test_function.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions anddd
 # limitations under the License.
 
-from grscheller.fp.function import partial, swap
+from grscheller.fp.function import partial, sequenced, swap
 from grscheller.fp.iterables import foldL, take
 
 class Test_function:
@@ -38,11 +38,23 @@ class Test_function:
                          lambda names, name: names + sep + name,
                          "").get()[len(sep):]
 
-        cast = ['Moe', 'Larry', 'Curlie', 'Shemp']
+        cast = ['Moe', 'Larry', 'Curlie', 'Shemp', 'Curlie Joe']
         stooges = names(3, ', ', cast)
         assert stooges == 'Moe, Larry, Curlie'
         stooges = names(1, ', ', cast)
         assert stooges == 'Moe'
         stooges = names(0, ', ', cast)
         assert stooges == ''
+
+        stooges3 = partial(names, 3, ' and ')
+        assert stooges3(cast) == 'Moe and Larry and Curlie'
+
+    def test_sequenced(self) -> None:
+        def compute(a: float, b: float, c: float, d: int) -> float:
+            return ( (a + b)*c ) ** d
+
+        data = [3.1, 4.2, 2.7, 5]
+        compute_seq = sequenced(compute)
+
+        assert compute(*data) == compute_seq(data)
 
